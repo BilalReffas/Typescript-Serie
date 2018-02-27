b26aae3fa9106b843bdf706f62e2f31cf51983a6
Bilal Karim Reffas <bilalreffas@googlemail.com>
2018-02-27 20:29:33 +0100

    added simple datatype example

---
+++ Primitve Datentypen/script.ts
@@ -0,0 +1,44 @@
+// let <Bezeichner>: <Type> = <Wert>;
+
+// Boolean Wahrheitswerte true oder false 
+let isHappy: boolean = true;
+
+isHappy = false;
+
+//console.log(typeof isHappy);
+
+//console.log(isHappy);
+
+//Konstante
+const success: boolean = true;
+
+// Number Zahlen
+let age: number = 5;
+
+// Berechnungen
+//console.log(age * 5);
+
+//console.log(age / 5);
+
+//console.log(age - 5);
+
+//console.log(age + 5);
+
+//console.log(typeof age);
+
+// String Zeichenketten
+const firstname: string = 'Müller';
+const message: string = 'Hallo';
+
+const example1: string = "Test";
+const example2: string = 'Test';
+const example3: string = `Test`;
+
+
+//console.log(message +" "+ firstname);
+//console.log(`${message} ${firstname}`);
+//console.log(typeof firstname);
+
+let isReady: boolean = true;
+
+console.log(typeof isReady);
