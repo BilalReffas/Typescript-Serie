175c6f8ddf236d8ec50666f47e2031b69a859cfc
Bilal Karim Reffas <bilalreffas@googlemail.com>
2018-03-17 19:12:27 +0100

    Arrays

---
+++ Komplexe Datentypen/script.ts
@@ -0,0 +1,11 @@
+const ages: number[] = [2,4,5,2,4,5,5];
+
+console.log(ages[0]);
+
+ages.push(2);
+
+console.log(ages);
+
+const names: string[] = ["Manfred", "Lisa", "Joachim", "Sabine"];
+
+console.log(names);
